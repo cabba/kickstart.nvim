1158845abd8c9e0f44f29e4c9ed34acacfd50f2b
Federico Cabassi <federico.cabassi@dilica.it>
2025-12-14 18:45:15 +0100

    experimenting with some colorschemes

--- init.lua
+++ init.lua
@@ -641,7 +641,7 @@ require('lazy').setup({
         rust_analyzer = {
           settings = {
             ['rust-analyzer'] = {
-              checkOnSave = {
+              check = {
                 command = 'clippy',
               },
             },
@@ -867,23 +867,56 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     -- vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+  -- {
+  --   'rrethy/base16-nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   init = function()
+  --     vim.o.termguicolors = true
+  --     vim.cmd.colorscheme 'base16-tokyo-night-stormm'
+  --   end,
+  -- },
+
+  {
+    'p00f/alabaster.nvim',
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.g.alabaster_dim_comments = true
+      vim.g.alabaster_floatborder = false
+      vim.o.termguicolors = true
+      vim.o.background = 'dark'
+      vim.cmd.colorscheme 'alabaster'
     end,
   },
+
+  -- {
+  --   'marko-cerovac/material.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   init = function()
+  --     vim.g.material_style = 'darker'
+  --     vim.cmd.colorscheme 'material'
+  --   end,
+  -- },
 
   -- { -- You can easily change to a different colorscheme.
   --   -- Change the name of the colorscheme plugin below, and then
